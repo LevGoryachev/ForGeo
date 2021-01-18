720b91201ae796897a5a687197c67f2547460512
Lev <l.goryachev@inbox.ru>
2021-01-18 09:00:00 +0300

    Change to Spring Boot (AppInit)

--- src/main/java/ru/goryachev/forgeo/AppInit.java
+++ src/main/java/ru/goryachev/forgeo/AppInit.java
@@ -1,24 +1,12 @@
 package ru.goryachev.forgeo;
 
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
-    }
+public class AppInit extends SpringBootServletInitializer {
 
-    //Config classes
     @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class[]{
-                Config.class
-        };
-    }
-
-    //servlet mapping
-    @Override
-    protected String[] getServletMappings() {
-        return new String[]{"/"};
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder appBuilder) {
+        return super.configure(appBuilder);
     }
 }
