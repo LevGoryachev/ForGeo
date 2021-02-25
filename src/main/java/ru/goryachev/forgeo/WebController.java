aae0f9ee030914de1c0cb47637020c648d486218
LevGoryachev <l.goryachev@inbox.ru>
2021-02-25 15:31:00 +0300

    Change to RestController, GetMappings

--- src/main/java/ru/goryachev/forgeo/WebController.java
+++ src/main/java/ru/goryachev/forgeo/WebController.java
@@ -1,24 +1,36 @@
 package ru.goryachev.forgeo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-
+@RestController
 public class WebController {
 
     @GetMapping("/main")
     public String mainPage () {
-        return "main_page";
+        return "APP info: ForGeo (by Lev Goryachev) webapp is based on SpringBoot, Hibernate, PostgreSQL; frontend module - ReactJS";
     }
 
-    @GetMapping("/one")
-    public String welcome () {
-        return "pageone";
+    @GetMapping("/constructions")
+    public String constructions () {
+        return "List of all constructions";
+    }
+/*
+    @GetMapping("/{name}")
+    public String getConstructionByName () {
+        return "Construction";
     }
 
-    @GetMapping("/two")
-    public String hello () {
-        return "pagetwo";
+    @GetMapping("/{id}")
+    public String getConstructionById () {
+        return "Construction";
     }
+
+*/
+
+    @GetMapping("/coordinates")
+    public String coordinates () {
+        return "List of all coordinates";
+    }
+
 }
