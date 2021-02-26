21dce0a7ee11d244189eb187b39d4622ee2f902c
LevGoryachev <l.goryachev@inbox.ru>
2021-02-26 14:17:00 +0300

    add empty implementations of ForGeoService

---
+++ src/main/java/ru/goryachev/forgeo/services/LocationService.java
@@ -0,0 +1,38 @@
+package ru.goryachev.forgeo.services;
+
+import ru.goryachev.forgeo.entities.BaseEntity;
+
+import java.util.List;
+
+public class LocationService implements ForGeoService {
+
+    @Override
+    public List getAll() {
+        return null;
+    }
+
+    @Override
+    public BaseEntity getById(int id) {
+        return null;
+    }
+
+    @Override
+    public BaseEntity getByName(String name) {
+        return null;
+    }
+
+    @Override
+    public void create(BaseEntity entity) {
+
+    }
+
+    @Override
+    public void update(BaseEntity entity) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+}
