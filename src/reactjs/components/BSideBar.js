e053fa8e785a395b1dd428aeaa08c86bf4d62d08
LevGoryachev <l.goryachev@inbox.ru>
2021-04-05 13:23:00 +0300

    Frontend: restructure packages (according to NextJS)

--- src/reactjs/components/BSideBar.js
+++ src/reactjs/components/BSideBar.js
@@ -6,6 +6,7 @@ class BSideBar extends React.Component {
         return (
             <div id="b_side_bar">
                 <h3>Lef side bar</h3>
+                <h3>React with NextJS based</h3>
                 <p id="copyrightline">ForGeo createdByLevGoryachev();</p>
             </div>
         );
--- src/reactjs/pages/index.js
+++ src/reactjs/pages/index.js
@@ -1,7 +1,18 @@
+import AHeader from "../components/AHeader";
+import BSideBar from "../components/BSideBar";
+import CMainBlock from "../components/CMainBlock";
+import DClear from "../components/DClear";
+import EFooter from "../components/EFooter";
+
+
 const Index = () => {
     return (
-        <div>
-            <h2>ForGeo - Next</h2>
+        <div id="container">
+            <AHeader />
+            <BSideBar />
+            <CMainBlock />
+            <DClear />
+            <EFooter />
         </div>
     );
 };
