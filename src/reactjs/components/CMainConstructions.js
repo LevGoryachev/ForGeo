01e39d1723190e6e100109937cd8676d8a717fb7
LevGoryachev <l.goryachev@inbox.ru>
2021-04-19 10:49:00 +0300

    Frontend: apply dynamic routing (List of Constructions)

--- src/reactjs/components/CMainConstructions.js
+++ src/reactjs/components/CMainConstructions.js
@@ -1,18 +1,32 @@
 import Headline from "./Headline";
+import Link from "next/link";
 
 const Constructions = (props) => {
 
     return (
         <div id="c_main">
             <Headline />
-            <h2>List of constructions:</h2>
-            <ul>
+            <table border="1" cellPadding="5">
+                <tr>
+                    <td width="100px">Database ID</td>
+                    <td width="200px">CODE-NUMBER</td>
+                    <td width="200px">Name of project</td>
+                </tr>
                 {props.getCon.map((construction) =>
-                    <li>
-                        {construction.id}, {construction.codeNumber}, {construction.name}: {construction.description}
-                    </li>
+                    <tr>
+                        <td width="100px">{construction.id}</td>
+                        <td width="200px">{construction.codeNumber}</td>
+                        <td width="500px">
+                            <Link href={`/constructions/${construction.id}`}>
+                                <a>{construction.name}</a>
+                            </Link>
+                        </td>
+                    </tr>
                 )}
-            </ul>
+            </table>
+
+            <h2>List of constructions:</h2>
+
         </div>
     );
 };
