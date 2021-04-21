e1499f702b4435ab539fc29234af469fd826ad5e
LevGoryachev <l.goryachev@inbox.ru>
2021-04-21 18:37:00 +0300

    Frontend: service_components (CRUD for constructions)

---
+++ src/reactjs/service_components/CRUDConstructions.js
@@ -0,0 +1,34 @@
+//import { apiUrl } from 'config';
+import {fetchWrapper} from "./FetchWrapper";
+
+
+export const CRUDconstructions = {
+    getAll,
+    getById,
+    create,
+    update,
+    delete: _delete
+};
+
+const baseUrl = `${apiUrl}/constructions`;
+
+function getAll() {
+    return fetchWrapper.get(baseUrl);
+}
+
+function getById(id) {
+    return fetchWrapper.get(`${baseUrl}/constructions/${id}`);
+}
+
+function create(params) {
+    return fetchWrapper.post(baseUrl, params);
+}
+
+function update(id, params) {
+    return fetchWrapper.put(`${baseUrl}/constructions/${id}`, params);
+}
+
+// prefixed with underscored because delete is a reserved word in javascript
+function _delete(id) {
+    return fetchWrapper.delete(`${baseUrl}/constructions/${id}`);
+}
