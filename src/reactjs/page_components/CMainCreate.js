7f8b49941f5eae59d280bf9578a430805b3e6698
LevGoryachev <l.goryachev@inbox.ru>
2021-05-05 10:39:00 +0300

    Frontend: "create"- add form, useForm hook

---
+++ src/reactjs/page_components/CMainCreate.js
@@ -0,0 +1,131 @@
+import Headline from "./Headline";
+import Link from "next/link";
+import React, { useContext, useState, useEffect } from 'react'
+import {ForGeoContext, getAsyncData, getSyncData, getSyncInitDataInMemory, getSyncState} from "../stores/store";
+import Constructions, {constructionDel, getOne, getStaticProps, newNumber, updateData} from "../pages/constructions";
+import {useForm} from "react-hook-form";
+
+
+
+const cMainConstructions = (props) => {
+
+    const {constructionsx, updConstructions} = useContext(ForGeoContext);
+
+    const [constructionsxState, updConstructionsState] = useState(constructionsx);
+
+    const [loading, setLoading] = useState(false);
+
+    /*useEffect(() => {
+        setLoading(true);
+        CRUDconstructions.getAll()
+            .then((x) => {
+                updConstructionsState(x);
+            })
+            .then((y) => console.log(y))
+            .catch((err) => {
+                console.log(err);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+
+    }, []);
+
+    if (loading) {
+        return (
+        <div id="c_main">
+            <Headline />
+            <p>Data is loading...</p>;
+        </div>
+        )
+    }*/
+
+        const { register, handleSubmit } = useForm();
+
+        function onSubmit(data) {
+            console.log(data);
+            // { username: 'test', email: 'test', password: 'test' }
+        }
+
+        return (
+            <div id="c_main">
+                <Headline />
+
+                <form onSubmit={handleSubmit(onSubmit)}>
+
+                        {/*<input title="ID" name="name" ref={register} placeholder="Name"/>
+                        <input title="Code-Number of construction" name="codeNumber" ref={register} placeholder="Code-number"/>
+                        <input title="Description" type="text" name="description" ref={register} placeholder="Description"/>*/}
+                    <h3>Set general information about the construction site:</h3>
+                    <table align="center" border="1" cellPadding="5">
+                        <tr>
+                            <td width="400px"><input title="ID" name="name" ref={register} placeholder="Name"/></td>
+                            <td width="400px"><input title="Code-Number of construction" name="codeNumber" ref={register} placeholder="Code-number"/></td>
+                        </tr>
+                    </table>
+                    <h3>Description:</h3>
+                    <table align="center" border="1" cellPadding="5">
+                        <td align="left" width="820px"><input title="Description" type="text" name="description" ref={register} placeholder="Description"/></td>
+                    </table>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        );
+
+    /*return (
+        <div id="c_main">
+            <Headline />
+            {/!*<p>{language}</p>*!/}
+
+            <table border="1" cellPadding="5">
+                <tr>
+                    <td width="100px">Database ID</td>
+                    <td width="200px">CODE-NUMBER</td>
+                    <td width="200px">Name of project</td>
+                    <td colSpan="2">EDIT DATA</td>
+                </tr>
+                {constructionsxState.map((construction) =>
+                    <tr>
+                        <td width="100px">{construction.id}</td>
+                        <td width="200px">{construction.codeNumber}</td>
+                        <td width="500px">
+                            <Link href={`/constructions/${construction.id}`}>
+                                <a>{construction.name}</a>
+                            </Link>
+                        </td>
+                        <td>
+                            <button onClick={() => {constructionDel(construction.id)
+                                .then(() => {
+                                CRUDconstructions.getAll()
+                                    .then((x) => {
+                                        updConstructionsState(x);
+                                    })
+                                    .then((y) => console.log(y))
+                                    .catch((err) => {
+                                        console.log(err);
+                                    });
+                                //window.location.href='/constructions'
+                            });}}>DELETE</button>
+                        </td>
+
+                    </tr>
+                )}
+
+                {/!*<tr>
+
+                        <td > {const nameInput = () = {<input title="ID" type="text" const name="name"></input>}}</td>
+                        <td ><input title="ID" type="text" name="codeNumber"></input></td>
+                        <td ><input title="ID" type="text" name="description"></input></td>
+
+                        <td colSpan="2"><input type="submit" value="ADD" onSubmit={() => {CRUDconstructions.create().then(() => {
+                            window.location.href='/constructions';
+                            console.log(nameInput);
+                        });}}/></td>
+
+                </tr>*!/}
+            </table>
+        </div>
+    );*/
+
+};
+export default cMainConstructions;
---
+++ src/reactjs/pages/constructions/create.js
@@ -0,0 +1,18 @@
+import AHeader from "../../page_components/AHeader";
+import BSideBar from "../../page_components/BSideBar";
+import DClear from "../../page_components/DClear";
+import EFooter from "../../page_components/EFooter";
+import CMainCreate from "../../page_components/CMainCreate";
+
+const Constructions = () => {
+    return (
+        <div id="container">
+            <AHeader />
+            <BSideBar />
+            <CMainCreate />
+            <DClear />
+            <EFooter />
+        </div>
+    );
+};
+export default Constructions;
