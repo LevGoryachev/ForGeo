ebe08f79f8d16af86285e588e2ecaaa80413bb5e
LevGoryachev <l.goryachev@inbox.ru>
2021-09-01 11:29:00 +0300

    Logging (Slf4j) AddressService

--- src/main/java/ru/goryachev/forgeo/services/AddressService.java
+++ src/main/java/ru/goryachev/forgeo/services/AddressService.java
@@ -1,5 +1,7 @@
 package ru.goryachev.forgeo.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.goryachev.forgeo.exceptions.EmptyListException;
@@ -22,38 +24,48 @@ public class AddressService {
     @Autowired
     private AddressRepo addressRepo;
 
-    private LocationRepo locationRepo;
+    private final Logger log = LoggerFactory.getLogger(AddressService.class);
 
     public List<Address> getAll(String type) throws EmptyListException {
+
+        if (log.isDebugEnabled()) {
+            log.debug("AddressService getAll()" + ", " + "params: " + type);
+        }
+
         if (type == null) {
             List<Address> allAddresses = addressRepo.findAll();
             if (allAddresses.isEmpty()) {
+                log.error("AddressRepo returned empty list");
                 throw new EmptyListException();
             }
             return allAddresses;
         }
         List<Address> allAddresses = addressRepo.findAllByType(type);
         if (allAddresses.isEmpty()) {
+            log.error("AddressRepo returned empty list");
             throw new EmptyListException();
         }
         return allAddresses;
     }
 
     public Address getById(Long id) throws EntityNotFoundException {
+        log.info("AddressService getById()");
         Address address = addressRepo.findById(id).orElseThrow(() -> new EntityNotFoundException(id));
         return address;
     }
 
     public Address create(Address address) {
-
+        log.info("AddressService create(address)");
         return addressRepo.save(address);
     }
 
     public Address update(Address modifiedAddress) {
+        log.info("AddressService create(address)");
         return addressRepo.save(modifiedAddress);
     }
 
     public void delete(Long id) {
+        log.info("AddressService delete(id)");
         addressRepo.deleteById(id);
     }
 }
